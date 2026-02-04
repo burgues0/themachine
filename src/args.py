d9e06afb32bb8262e63e44ef8353a5ab17a550b0
burgues0 <victordpp@outlook.com>
2026-02-03 23:01:46 -0300

    add: default args

--- src/args.py
+++ src/args.py
@@ -4,8 +4,8 @@ def get_args():
     parser = argparse.ArgumentParser(prog='themachine', description='Another yt-dlp based script, focused on "searching" full albums with quality.')
 
     parser.add_argument('-u', '--url', required=True, help="Reference URL of choice.")
-    parser.add_argument('-e', '--extension', choices=['mp3', 'flac', 'wav'], help="Determine the file extension of the output.")
-    parser.add_argument('-b', '--bitrate', help="Determine the bitrate of the output.")
+    parser.add_argument('-e', '--extension', choices=['mp3', 'flac', 'wav'], default='flac', help="Determine the file extension of the output.")
+    parser.add_argument('-b', '--bitrate', default='128', help="Determine the bitrate of the output.")
     parser.add_argument('-c', '--check', action='store_true', help="Check if the output matches the expected result.")
     
     return parser.parse_args()
